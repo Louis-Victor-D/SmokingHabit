21b4df68797d2a7414ede41bed5041102a5e6937
Doodle-Q <r1059542@student.thomasmore.be>
2025-05-31 17:27:40 +0200

    fix: fixed the storage issue

--- details.js
+++ details.js
@@ -1,7 +1,5 @@
-const headerEl = document.getElementById('currentDateHeader');
-const today = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-headerEl.textContent = today.toLocaleDateString(undefined, options);
+const today = new Date().toISOString().split("T")[0];
+
 let smokingData = JSON.parse(localStorage.getItem("smokingData")) || {};
 let count = smokingData[today] || 0;
 
@@ -11,6 +9,8 @@ const numberDisplay = document.querySelector('.number');
 const textDisplay = document.querySelector(".text");
 const btnPlus = document.querySelector('.btnPlus');
 const btnMinus = document.querySelector('.btnMinus');
+
+document.getElementById("currentDateHeader").textContent = today;
 
 function updateDisplay() {
   numberDisplay.textContent = `🚬 ${count}/${max}`;
